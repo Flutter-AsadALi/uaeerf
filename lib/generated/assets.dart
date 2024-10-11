3cd70d1c41b7e2e56008f87d39297b3923f5359f
Asad Ali <it@smuccivapes.com>
2024-10-11 23:16:07 +0400

    new

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -3,7 +3,6 @@ class Assets {
   Assets._();
 
   static const String assetsFontsMadimiOneRegular = 'assets/fonts/MadimiOne-Regular.ttf';
-  static const String assetsImagesLoader = 'assets/images/loader.gif';
   static const String fontsMadimiOneRegular = 'assets/fonts/MadimiOne-Regular.otf';
   static const String iconsAppLogo = 'assets/icons/app_logo.png';
   static const String imagesAppLogo = 'assets/images/app_logo.png';
@@ -11,11 +10,5 @@ class Assets {
   static const String imagesDashLogo = 'assets/images/dash_logo.png';
   static const String imagesLoader = 'assets/images/loader.gif';
   static const String imagesNewLogo = 'assets/images/new_logo.png';
-  static const String lottieFilesDeliverdOrder = 'assets/lottie_files/deliverd_order.json';
-  static const String lottieFilesEmptyData = 'assets/lottie_files/empty_data.json';
-  static const String lottieFilesInfo = 'assets/lottie_files/info.json';
-  static const String lottieFilesPickUp = 'assets/lottie_files/pick_up.json';
-  static const String lottieFilesStartJob = 'assets/lottie_files/start_job.json';
-  static const String ringRingtone = 'assets/ring/ringtone.mp3';
 
 }
